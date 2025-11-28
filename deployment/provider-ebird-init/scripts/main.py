fa88b502943217b397d51d6a6d667d2f35970428
Maria Ortega <maria3108x@gmail.com>
2025-11-28 16:35:05 +0100

    Update ebirt init script

--- deployment/provider-ebird-init/scripts/main.py
+++ deployment/provider-ebird-init/scripts/main.py
@@ -25,7 +25,7 @@ ASSETS_URL = f"{BASE_URL}/management/v3/assets-cbm"
 SAMPLES_URL = f"{BASE_URL}/management/v3/assets-cbm"
 ASSETS_IDS = ["1"]
 SAMPLES_IDS = ["2"]
-
+OPEN_POLICY_ID = "always-true"
 
 
 ASSET_ID_MAP = {
@@ -35,7 +35,6 @@ ASSET_ID_MAP = {
 
 
 POLICIES = [
-    {"id": "open-policy","policy_file": "open_policy.json", "contract_file": "open_contract.json", "assets_id_key": "SAMPLES_IDS"},
     {"id": "basic-policy", "policy_file": "basic_policy.json", "contract_file": "basic_contract.json", "assets_id_key": "ASSETS_IDS"},
 ]
 
@@ -81,8 +80,8 @@ def create_assets_for_region_code(file, metadata, url_template, name, desc, resp
     Create asset for each province in spain a JSON file.
     """
     with open(file, 'r', encoding="utf8") as file:
-         data = json.load(file)
-         for item in data:
+        data = json.load(file)
+        for item in data:
             code = item["code"]
             province_name = item["name"]
             print(f"\033[32m{province_name}\033[0m")
@@ -93,39 +92,39 @@ def create_assets_for_region_code(file, metadata, url_template, name, desc, resp
             url = url_template.format(code=code)
             asset_id =  create_asset_and_sample(metadata, url, asset_name, asset_desc, province_name, response_type)
             if asset_id:
-                    ASSETS_IDS.append(asset_id)
-                    SAMPLES_IDS.append(f"sample-{asset_id}")
-                    logging.info(f"CREATED ASSET {asset_id} AND SAMPLE sample-{asset_id}")
+                ASSETS_IDS.append(asset_id)
+                SAMPLES_IDS.append(f"sample-{asset_id}")
+                logging.info(f"CREATED ASSET {asset_id} AND SAMPLE sample-{asset_id}")
 
 
 def create_asset_no_region_code(metadata, url, name, desc, response_type ="json"):
-      """
-      Create asset for no region code endpoint
-      """
-      asset_id =  create_asset_and_sample(metadata, url, desc, name, response_type)
-      if asset_id:
+    """
+    Create asset for no region code endpoint
+    """
+    asset_id =  create_asset_and_sample(metadata, url, desc, name, response_type)
+    if asset_id:
         ASSETS_IDS.append(asset_id)
         SAMPLES_IDS.append(f"sample-{asset_id}")
         logging.info(f"CREATED ASSET {asset_id} AND SAMPLE sample-{asset_id}")
 
 
 def create_asset_taxonomic_groups(metadata, url_template, name, desc, response_type ="json"):
-      """
-      Create asset for taxonomic groups endpoint
-      """
-      groups = ["ebird", "merlin"]
-      for group in groups:
-         url = url_template.format(code=group)
-         name = name.format(code=group)
-         asset_name = re.sub(r'\s+', '-', name.lower())
-         desc = desc.format(code=group)
+    """
+    Create asset for taxonomic groups endpoint
+    """
+    groups = ["ebird", "merlin"]
+    for group in groups:
+        url = url_template.format(code=group)
+        name = name.format(code=group)
+        asset_name = re.sub(r'\s+', '-', name.lower())
+        desc = desc.format(code=group)
 
-         logging.info(f"taxonomic {metadata},{url}, {name} {group} ")
-         asset_id =  create_asset_and_sample(metadata, url, asset_name, desc, response_type)
-         if asset_id:
-          ASSETS_IDS.append(asset_id)
-          SAMPLES_IDS.append(f"sample-{asset_id}")
-          logging.info(f"CREATED ASSET {asset_id} AND SAMPLE sample-{asset_id}")
+        logging.info(f"taxonomic {metadata},{url}, {name} {group} ")
+        asset_id =  create_asset_and_sample(metadata, url, asset_name, desc, response_type)
+        if asset_id:
+            ASSETS_IDS.append(asset_id)
+            SAMPLES_IDS.append(f"sample-{asset_id}")
+            logging.info(f"CREATED ASSET {asset_id} AND SAMPLE sample-{asset_id}")
 
 
 ASSET_CONFIGS = [
@@ -151,35 +150,36 @@ ASSET_CONFIGS = [
 
 
 def create_policy(filename):
-   filepath = os.path.join("policies", filename)
-   with open(filepath) as f:
-    data = json.load(f)
-   created = post_json(POLICY_DEFINITIONS_URL, data, X_API_KEY)
-   if not created:
-        return None
-   else:
-        return data['@id']
-
-
-def create_contract_definition(filename, access_policy_id, contract_policy_id, asset_ids):
-   filepath = os.path.join("contracts", filename)
-   with open(filepath) as f:
-    data = json.load(f)
-    data["accessPolicyId"] = access_policy_id
-    data["contractPolicyId"] = contract_policy_id
-    data["assetsSelector"][0]["operandRight"] = asset_ids
-    json.dumps(data)
-    created = send_request(CONTRACT_DEFINITIONS_URL, data, X_API_KEY)
+    filepath = os.path.join("policies", filename)
+    with open(filepath) as f:
+        data = json.load(f)
+    created = post_json(POLICY_DEFINITIONS_URL, data, X_API_KEY)
     if not created:
         return None
     else:
         return data['@id']
 
 
+def create_contract_definition(filename, access_policy_id, contract_policy_id, asset_ids):
+    filepath = os.path.join("contracts", filename)
+    with open(filepath) as f:
+        data = json.load(f)
+        data["accessPolicyId"] = access_policy_id
+        data["contractPolicyId"] = contract_policy_id
+        data["assetsSelector"][0]["operandRight"] = asset_ids
+        json.dumps(data)
+        created = send_request(CONTRACT_DEFINITIONS_URL, data, X_API_KEY)
+        if not created:
+            return None
+        else:
+            return data['@id']
+
+
 
 def create_asset_and_sample(metadata, url, name, desc, province=None, response_type="json"):
     asset_id, asset_payload = build_asset(url, metadata, desc, name, API_KEY, province)
     created = send_request(ASSETS_URL, asset_payload, X_API_KEY)
+    print(created)
     if not created:
         return None
 
@@ -202,13 +202,19 @@ def main():
 
 
 
+    #Contract for samples
+    contract_sample_id = create_contract_definition("open_contract.json", OPEN_POLICY_ID, OPEN_POLICY_ID, SAMPLES_IDS)
+    logging.info(f"\033[1;34mCREATED CONTRACT FOR SAMPLES {contract_sample_id}\033[0m")
+
     for policy in POLICIES:
         policy_id = create_policy(policy["policy_file"])
         logging.info(f"\033[1;34mCREATED POLICY {policy_id}\033[0m")
 
         if policy_id:
-            contract_id = create_contract_definition(policy["contract_file"], policy_id, policy_id, ASSET_ID_MAP[policy["assets_id_key"]])
+            contract_id = create_contract_definition(policy["contract_file"], OPEN_POLICY_ID, policy_id, ASSET_ID_MAP[policy["assets_id_key"]])
             logging.info(f"\033[1;34mCREATED CONTRACT {contract_id}\033[0m")
+
+
 
 
 if __name__ == "__main__":
